94ff300e649e1126216f0d9939d2cc6f7639d155
bhupi1979 <bhupendrapaliwal1983@gmail.com>
2023-12-26 16:45:41 +0530

    frontend chage

--- src/components/Admin/Productcategory.jsx
+++ src/components/Admin/Productcategory.jsx
@@ -8,6 +8,7 @@ import DataTable from "react-data-table-component";
 import { Modelupdateproductcategory } from "./Modelupdateproductcategory";
 import jsPDF from 'jspdf';
 import 'jspdf-autotable'
+
 export function Productcategory(){
     const [errors, setErrors] = useState({});
     const [searchQuery, setSearchQuery] = useState('');
@@ -106,7 +107,7 @@ async function addproductcategory(){
             {
               name: 'Action',
               cell:(row)=>(<button onClick={() => setEditId(row._id) } data-bs-toggle="modal" data-bs-target="#exampleModal"><span  data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit Row">
-              <svg className="tooltip" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16" >
+              <svg className="tooltip" xmlns="http://www.w3.org/2000/svg" width="30" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16" >
               <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
             </svg></span></button>)
               
@@ -114,7 +115,7 @@ async function addproductcategory(){
             {
                 name: 'Action',
                 cell:(row)=>( <span  onClick={() => handleDelete(row._id)}  data-bs-toggle="tooltip" data-bs-placement="bottom" title="delete Row" >
-                <svg xmlns="http://www.w3.org/2000/svg"   width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16"  data-bs-toggle="tooltip" data-bs-placement="top" title="Delete Row" >
+                <svg xmlns="http://www.w3.org/2000/svg"   width="30" height="20" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16"  data-bs-toggle="tooltip" data-bs-placement="top" title="Delete Row" >
                 <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
                 </svg></span>
             )
@@ -219,7 +220,23 @@ const validateForm = () => {
         return isValid
 }
 /*********validation ends herer */
-
+/*********custom styles for rows datatable */
+const customStyles = {
+  headCells: {
+    style: {
+       fontSize:'26px',
+       color:'black',
+       backgroundColor:"#fefefe"
+    },
+},
+  rows: {
+    style: {
+      fontSize: '20px', // Set text size for all rows
+      color:'#800000',
+      backgroundColor:'lightblue'
+    },
+  },
+};
 
 
 
@@ -245,12 +262,12 @@ const validateForm = () => {
                     <div className="col-lg-7">
                         <h1 className="text-center">The Product-Category_Detail</h1>
                         <CSVLink data={data} filename={"data.csv"}>
-                        <button>ExportCSV</button>
+                        <button className="p-1">ExportCSV</button>
                     </CSVLink>
-                    <button onClick={handlePrint}>ExportPdf</button>
+                    <button className="p-1" onClick={handlePrint}>ExportPdf</button>
 
-                    <button onClick={handlePrint1}>PRint</button>
-                        {data?<div style={{ display: 'block' }} ref={componentRef} >
+                    <button className="p-1" onClick={handlePrint1}>PRINT</button>
+                        {data?<div style={{ display: 'block' }} className="border border-4 border-danger" ref={componentRef} >
       
       <DataTable
         
@@ -261,6 +278,8 @@ const validateForm = () => {
         pointerOnHover
         pagination
         subHeader
+        customStyles={customStyles}
+        
         subHeaderComponent={
           <input
           width="10rem"
